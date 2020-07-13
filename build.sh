ac574d77a13cfbc4f11c63a734f11eb82a7e07ec
Marvin <MarvAmBass@users.noreply.github.com>
2020-07-13 14:57:41 +0200

    removed index.html generation

--- build.sh
+++ build.sh
@@ -34,22 +34,6 @@ done
 echo
 echo
 
-for folder in $(find . -iname "index.html" -exec sh -c 'echo "{}" | sed "s,[^/]*/index.html,,g"' \; | sort | uniq); do
-  echo -n "indexing $folder ... "
-  echo '<html><head></head><body><h1>Stagit - Projects</h1><hr><ul>' > "$folder/index.html"
-
-  for subfolder in $(find "$folder" -iname "index.html" -exec sh -c 'echo "{}"' \; | sort | uniq | grep -v '^'"$folder"'index.html$'); do
-    subfolder_name=$(echo "$subfolder" | sed 's,^\./,,g' | sed 's,/index.html,,g')
-    echo '<li><a href="'"$subfolder"'">'"$subfolder_name"'</a></li>' >> "$folder/index.html"
-  done 
-
-  echo '</ul></body></html>' >> "$folder/index.html"
-  echo "done"
-done
-
-echo
-echo
-
 echo -n "set permissions ..."
 chmod a+rwx -R *
 echo "done"
